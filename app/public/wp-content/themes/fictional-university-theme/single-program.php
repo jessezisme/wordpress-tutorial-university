c4f49540d8772167101da1986b3694f363859cee
jessez <jessezdeveloper@gmail.com>
2022-12-18 17:19:44 -0500

    Section 9.40: Professors Post Type.

--- app/public/wp-content/themes/fictional-university-theme/single-program.php
+++ app/public/wp-content/themes/fictional-university-theme/single-program.php
@@ -25,6 +25,38 @@ while (have_posts()) {
         </div>
 
         <?php
+        $relatedProfessors = new WP_Query(array(
+            'posts_per_page' => -1,
+            'post_type' => 'professor',
+            'orderby' => 'title',
+            'order' => 'asc',
+            'meta_query' => array(
+                array(
+                    'key' => 'related_programs',
+                    'compare' => 'LIKE',
+                    'value' => '"' . get_the_id() . '"'
+                )
+            )
+        ));
+
+        if ($relatedProfessors->have_posts()) {
+            echo '<hr class="section-break">';
+            echo '<h2 class="headline headline--medium">' . get_the_title() . ' Professor(s)</h2>';
+        }
+
+        if ($relatedProfessors->have_posts()) {
+            while ($relatedProfessors->have_posts()) {
+                $relatedProfessors->the_post();
+        ?>
+                <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+        <?php
+            }
+        }
+        ?>
+
+        <?php wp_reset_postdata(); ?>
+
+        <?php
         $today = date('Ymd');
         $homepageEvents = new WP_Query(array(
             'posts_per_page' => 2,
